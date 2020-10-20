735c1ca4e81f7f58186fa71c5e0fb091534fa4ff
reb <reboumans@gmail.com>
2020-10-20 06:17:53 -0400

    Stub for the Rust workshop's Fruit Vote assignment

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#[derive(Debug, PartialEq, Eq)] // derive auto-implements simple features, like debug printing and comparisons
+struct Fruit {
+    name: String,
+    amount: u32,
+}
+
+fn convert_to_fruit(line: &str) -> Result<Fruit, &str> {
+    Err("Function not implemented")
+}
+
+#[test]
+fn test_convert_to_fruit() {
+    let fruit = convert_to_fruit("banana, 21");
+
+    let name = "banana".to_string();
+    let amount = 21;
+    let expected = Ok(Fruit { name, amount });
+    assert_eq!(fruit, expected);
+}
+
+fn main() {
+    println!("Hello Rust lovers!");
+}
